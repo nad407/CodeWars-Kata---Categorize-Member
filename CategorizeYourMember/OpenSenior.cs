509231676c8b3b58282b89dd1f330048a50fae5e
nad407 <37850036+nad407@users.noreply.github.com>
2018-05-21 17:19:46 +0200

    CodeWars Kata using C#

---
+++ CategorizeYourMember/OpenSenior.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CategorizeYourMember
+{
+    class OpenSenior
+    {
+        public static IEnumerable<string> OpenOrSenior(int[][] data)
+        {
+            List<string> category = new List<string>();
+            int i = 0;
+
+            foreach (int[] person in data)
+            {
+                if ((person[0] >= 55) & (person[1] > 7))
+                {
+                    category.Add("Senior");
+                }
+                else
+                {
+                    category.Add("Open");
+                }
+                i++;
+            }
+
+            return category;
+        }
+    }
+}
---
+++ CategorizeYourMember/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CategorizeYourMember
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            IEnumerable<string> cat;
+            int[][] data = new[] { new[] { 45, 12 }, new[] { 55, 21 }, new[] { 19, 2 }, new[] { 104, 20 } };
+            cat = OpenSenior.OpenOrSenior(data);
+        }
+    }
+}
